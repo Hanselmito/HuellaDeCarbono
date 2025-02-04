79d3dc8cfedf6c107028d815b326d0034f22153d
Hanselmito <151913571+Hanselmito@users.noreply.github.com>
2025-02-04 11:01:55 +0100

    Generador PDF mejorado

--- src/main/java/com/github/Hanselmito/utils/PDFReportGenerator.java
+++ src/main/java/com/github/Hanselmito/utils/PDFReportGenerator.java
@@ -1,20 +1,26 @@
 package com.github.Hanselmito.utils;
 
+import com.github.Hanselmito.entities.Habito;
 import com.github.Hanselmito.entities.Huella;
 import com.github.Hanselmito.entities.Recomendacion;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PDFReportGenerator {
 
-    public void generateReport(String dest, List<Huella> huellas, List<Recomendacion> recomendaciones) throws IOException {
+    public void generateReport(String dest, List<Huella> huellas, List<Recomendacion> recomendaciones, List<Habito> habitos) throws IOException {
         File file = new File(dest);
         file.getParentFile().mkdirs();
 
@@ -24,25 +30,54 @@ public class PDFReportGenerator {
 
         document.add(new Paragraph("Reporte de Huella de Carbono"));
 
-        Table table = new Table(new float[]{4, 4, 4, 4});
-        table.addHeaderCell("Fecha");
-        table.addHeaderCell("Valor");
-        table.addHeaderCell("Actividad");
-        table.addHeaderCell("Unidad");
+        Map<String, List<Huella>> huellasPorActividad = huellas.stream()
+                .collect(Collectors.groupingBy(huella -> huella.getIdActividad().getNombre()));
 
-        for (Huella huella : huellas) {
-            table.addCell(huella.getFecha().toString());
-            table.addCell(huella.getValor().toString());
-            table.addCell(huella.getIdActividad().getNombre());
-            table.addCell(huella.getUnidad());
+        for (Map.Entry<String, List<Huella>> entry : huellasPorActividad.entrySet()) {
+            List<Huella> huellasDeActividad = entry.getValue();
+
+            Table table = new Table(new float[]{1, 4, 4, 4, 4});
+            table.setWidth(100);
+
+            table.addHeaderCell("Imagen");
+            table.addHeaderCell("Usuario");
+            table.addHeaderCell("Actividad");
+            table.addHeaderCell("Valor");
+            table.addHeaderCell("Fecha");
+
+            for (Huella huella : huellasDeActividad) {
+                Image image = new Image(ImageDataFactory.create(getClass().getResource("/com/github/Hanselmito/Icon/Huella.png")));
+                table.addCell(new Cell().add(image.setAutoScale(true)));
+
+                table.addCell(new Cell().add(new Paragraph(huella.getIdUsuario().getNombre())));
+                table.addCell(new Cell().add(new Paragraph(huella.getIdActividad().getNombre())));
+                table.addCell(new Cell().add(new Paragraph(huella.getValor().toString() + " " + huella.getUnidad())));
+                table.addCell(new Cell().add(new Paragraph(huella.getFecha().toString())));
+            }
+
+            document.add(table);
         }
-
-        document.add(table);
 
         document.add(new Paragraph("Recomendaciones"));
 
-        for (Recomendacion recomendacion : recomendaciones) {
-            document.add(new Paragraph(recomendacion.getDescripcion()));
+        for (Habito habito : habitos) {
+            String habitoInfo = String.format("Actividad: %s, Frecuencia: %d, Tipo: %s, Última Fecha: %s",
+                    habito.getIdActividad().getNombre(),
+                    habito.getFrecuencia(),
+                    habito.getTipo().toString(),
+                    habito.getUltimaFecha().toString());
+            document.add(new Paragraph(habitoInfo));
+
+            List<Recomendacion> recomendacionesPorHabito = recomendaciones.stream()
+                    .filter(recomendacion -> recomendacion.getIdCategoria().getId().equals(habito.getIdActividad().getIdCategoria().getId()))
+                    .collect(Collectors.toList());
+
+            int index = 1;
+            for (Recomendacion recomendacion : recomendacionesPorHabito) {
+                String recomendacionInfo = String.format("%d- %s", index, recomendacion.getDescripcion());
+                document.add(new Paragraph(recomendacionInfo));
+                index++;
+            }
         }
 
         document.close();
